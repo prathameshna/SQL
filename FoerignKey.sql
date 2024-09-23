9602d547db4304711b19d532b95cb5d89d72c1f5
prathameshna <158805916+prathameshna@users.noreply.github.com>
2024-09-23 14:34:18 +0530

    commit

--- FoerignKey.sql
+++ FoerignKey.sql
@@ -27,3 +27,26 @@ values
 (102, "shyam",75,"B", "satara"),
 (103, "ramesh",80,"A", "dahiwadi"),
 (104, "suresh",83,"A", "shirval");
+
+-- CASCADING FK :-The CASCADE rule of the foreign key states that when any entry is deleted from the parent table then all the dependent rows in the child table should also get deleted.
+create table teacher(
+id int primary key,
+name varchar(50),
+dept_id int,
+foreign key (dept_id) references dept(id)
+on update cascade
+on delete cascade
+); 
+
+-- ADD COLUMN this query use add a new column in existing table.
+alter table student
+ADD column age int;
+
+-- DROP COLUMN this query use delete a existing column in existing table.
+alter table student
+DROP column age;
+
+-- RENAME TO are use for rename table name.
+alter table student
+RENAME TO stud;
+
